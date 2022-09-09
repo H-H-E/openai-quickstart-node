66115f2e7006a69f78fb0d60a3030b3cb3b617de
H-H-E <husseinhelafifi@gmail.com>
2022-09-09 08:42:11 -0700

    works with my api

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -8,21 +8,28 @@ const openai = new OpenAIApi(configuration);
 export default async function (req, res) {
   const completion = await openai.createCompletion({
     model: "text-davinci-002",
-    prompt: generatePrompt(req.body.animal),
-    temperature: 0.6,
+    prompt: generatePrompt(req.body.topic),
+    temperature: 0.7,
+    max_tokens: 500,
+  
   });
   res.status(200).json({ result: completion.data.choices[0].text });
 }
 
-function generatePrompt(animal) {
-  const capitalizedAnimal =
-    animal[0].toUpperCase() + animal.slice(1).toLowerCase();
-  return `Suggest three names for an animal that is a superhero.
+function generatePrompt(topic) {
+  const capitalizedtopic =
+    topic[0].toUpperCase() + topic.slice(1).toLowerCase();
+  return `write me some hilarious jokes  
 
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: ${capitalizedAnimal}
-Names:`;
+topic:unemployment, work
+joke:I know a lot of jokes about unemployed people but none of them work.
+
+topic: deaf
+joke: Say what you want about deaf people.  
+topics: war, veteran, cooking
+joke: A soldier survived mustard gas in battle, and then pepper spray by the police. He's now a seasoned veteran.
+topics: injury
+joke: I told my doctor that I broke my arm in two places. He told me to stop going to those places.
+topic: ${capitalizedtopic}
+joke:`;
 }
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Head from "next/head";
-import { useState } from "react";
+import {
+  useState
+} from "react";
 import styles from "./index.module.css";
 
 export default function Home() {
-  const [animalInput, setAnimalInput] = useState("");
+  const [topicInput, settopicInput] = useState("");
   const [result, setResult] = useState();
 
   async function onSubmit(event) {
@@ -13,35 +15,62 @@ export default function Home() {
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ animal: animalInput }),
+      body: JSON.stringify({
+        topic: topicInput
+      }),
     });
     const data = await response.json();
     setResult(data.result);
-    setAnimalInput("");
+    settopicInput("");
   }
 
-  return (
-    <div>
-      <Head>
-        <title>OpenAI Quickstart</title>
-        <link rel="icon" href="/dog.png" />
-      </Head>
+  return ( <
+    div >
+    <
+    Head >
+    <
+    title > OpenAI Quickstart < /title> <
+    link rel = "icon"
+    href = "/lol.png" / >
+    <
+    /Head>
 
-      <main className={styles.main}>
-        <img src="/dog.png" className={styles.icon} />
-        <h3>Name my pet</h3>
-        <form onSubmit={onSubmit}>
-          <input
-            type="text"
-            name="animal"
-            placeholder="Enter an animal"
-            value={animalInput}
-            onChange={(e) => setAnimalInput(e.target.value)}
-          />
-          <input type="submit" value="Generate names" />
-        </form>
-        <div className={styles.result}>{result}</div>
-      </main>
-    </div>
+    <
+    main className = {
+      styles.main
+    } >
+    <
+    img src = "/lol.png"
+    className = {
+      styles.icon
+    }
+    /> <
+    h3 > Ask
+    for a joke < /h3> <
+    form onSubmit = {
+      onSubmit
+    } >
+    <
+    input type = "text"
+    name = "topic"
+    placeholder = "Enter a joke topic"
+    value = {
+      topicInput
+    }
+    onChange = {
+      (e) => settopicInput(e.target.value)
+    }
+    /> <
+    input type = "submit"
+    value = "Generate joke" / >
+    <
+    /form> <
+    div className = {
+      styles.result
+    } > {
+      result
+    } < /div> < /
+    main > <
+    /div>
   );
 }
